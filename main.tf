bc1d5335336aa263c4a2b8a35f220d87f38c34a4
jaabanks <86501922+jaabanks@users.noreply.github.com>
2021-09-27 19:30:23 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "project-ecs-cluster"
+
+  capacity_providers = ["FARGATE_SPOT", "FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+  }
+
+  setting {
+    name  = "containerInsights"
+    value = "disabled"
+  }
+}
+
+module "ecs-fargate" {
+  source  = "umotif-public/ecs-fargate/aws"
+  version = "~> 6.1.0"
+
+  name_prefix        = "ecs-fargate-example"
+  vpc_id             = "vpc-a28be9df"
+  private_subnet_ids = ["subnet-e90052b6"]
+
+  cluster_id = aws_ecs_cluster.cluster.id
+
+  task_container_image   = "centos"
+  task_definition_cpu    = 256
+  task_definition_memory = 512
+
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+
+  target_groups = [
+    {
+      target_group_name = "tg-fargate-example"
+      container_port    = 80
+    }
+  ]
+
+  health_check = {
+    port = "traffic-port"
+    path = "/"
+  }
+
+  tags = {
+    Environment = "test"
+    Project     = "Test"
+  }
+}
